b8f446b4eab966f3c7f80bfab03372acac89957c
Gurov540 <70337278+Gurov540@users.noreply.github.com>
2025-11-05 23:11:46 +0300

    add input

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { Button } from "@/shared/ui/Button";
+import { Input } from "@/shared/ui/Input";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Button>Кнопка</Button>
+    <Input color="primary"></Input>
+  </StrictMode>
+);
